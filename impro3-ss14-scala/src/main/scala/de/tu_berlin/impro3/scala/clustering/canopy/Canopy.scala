832c2c1fb64fef0fba46dd78014c2bde36842f99
Oresti Konomi <konomi.oresti@gmail.com>
2014-05-09 23:12:47 +0200

    Added parameter parsing

--- impro3-ss14-scala/src/main/scala/de/tu_berlin/impro3/scala/clustering/canopy/Canopy.scala
+++ impro3-ss14-scala/src/main/scala/de/tu_berlin/impro3/scala/clustering/canopy/Canopy.scala
@@ -5,6 +5,10 @@ import _root_.de.tu_berlin.impro3.scala.Algorithm
 
 object Canopy {
 
+  // argument names
+  val KEY_T1 = "T1"
+  val KEY_T2 = "T2"
+
   class Config extends Algorithm.Config[Canopy] {
 
     // algorithm names
@@ -15,13 +19,34 @@ object Canopy {
       // get common setup
       super.setup(parser)
       // add options (prefixed with --)
+      parser.addArgument(s"-${Canopy.KEY_T1}")
+        .`type`[Integer](classOf[Integer])
+        .dest(Canopy.KEY_T1)
+        .metavar("T1")
+        .help("T1 threshold")
+
+      parser.addArgument(s"-${Canopy.KEY_T2}")
+        .`type`[Integer](classOf[Integer])
+        .dest(Canopy.KEY_T2)
+        .metavar("T2")
+        .help("T2 threshold")
+
       // add defaults for options
+      parser.setDefault(Canopy.KEY_T1, new Integer(7))
+      parser.setDefault(Canopy.KEY_T2, new Integer(3))
     }
   }
 
 }
 
 class Canopy(args: Map[String, Object]) extends Algorithm(args) {
+  // algorithm specific parameters
+  val T1 = arguments.get(Canopy.KEY_T1).get.asInstanceOf[Int]
+  val T2 = arguments.get(Canopy.KEY_T2).get.asInstanceOf[Int]
+  // Check on parameter constraints
+  if (T1 < T2) {
+    throw new IllegalArgumentException("T1=" + T1 + " needs to be grater than T2=" + T2)
+  }
 
   def run(): Unit = {}
 }
