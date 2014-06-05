112d3a5d711ed31132cf0ebd95ffb2020f1b4b04
Christoph Viebig <git@christoph-viebig.de>
2014-06-05 13:45:26 +0200

    Add 0.01 percent sample of the Higgs data set
    The sampling was performed with a simple stratosphere sampling job.
    
    Use the following parameters to resample:
    RunSampling HIGGS.csv HIGGS-0.0001.csv 0.0001
    
    Be aware that the results are currently not reproducible as the seed
    is not configurable. In a distributed environment with a varying number
    of parallel workers it is also necessary to use a pseudo random number
    generator with skipValue functionality otherwise the result will again
    be not reproducible.

---
+++ impro3-ss14-stratosphere/src/main/scala/de/tu_berlin/impro3/stratosphere/utility/Sampling.scala
@@ -0,0 +1,54 @@
+package de.tu_berlin.impro3.stratosphere.utility
+
+import scala.util.Random
+
+import _root_.eu.stratosphere.client.LocalExecutor
+import _root_.eu.stratosphere.api.common.{Program, ProgramDescription}
+
+import _root_.eu.stratosphere.api.scala._
+import _root_.eu.stratosphere.api.scala.operators._
+
+class Sampling extends Program with ProgramDescription with Serializable {
+
+  def getScalaPlan(higgsInput: String, higgsOutput: String, size: Double) = {
+
+    val higgsData = TextFile(higgsInput).filter( x => Random.nextDouble < size )
+
+    val output = higgsData.write(higgsOutput, DelimitedOutputFormat({ line => line }))
+
+    new ScalaPlan(Seq(output), "Sampling")
+  }
+
+
+  /**
+   * The program entry point for the packaged version of the program.
+   *
+   * @param args The command line arguments, including, consisting of the following parameters:
+   *             <input> <output> <chance>"
+   * @return The program plan of the sample example program.
+   */
+  override def getPlan(args: String*) = {
+    getScalaPlan(args(0), args(1), args(2).toDouble)
+  }
+
+  override def getDescription() = {
+    "Parameters: <input> <output> <chance>"
+  }
+
+}
+
+/**
+ * Entry point to make the example standalone runnable with the local executor
+ */
+object RunSampling {
+
+  def main(args: Array[String]) {
+    val sampling = new Sampling
+    if (args.size < 2) {
+      println(sampling.getDescription)
+      return
+    }
+    val plan = sampling.getScalaPlan(args(0), args(1), args(2).toDouble)
+    LocalExecutor.execute(plan)
+  }
+}
