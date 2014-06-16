dc1639aff16ad8bd8a2c2ccaa8eb98fbe672976f
cyuw <yuwen.chen99@gmail.com>
2014-06-16 22:21:12 +0200

    Added Kmeans++ for 1-dimensional data in user-defined type, added test for Bag Of Words data set, use termination criteria instead of fixed iteration number

---
+++ impro3-ss14-stratosphere/src/main/java/de/tu_berlin/impro3/stratosphere/clustering/kmeanspp/KMeansppBagOfWords.java
@@ -0,0 +1,159 @@
+package de.tu_berlin.impro3.stratosphere.clustering.kmeanspp;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import de.tu_berlin.impro3.stratosphere.clustering.kmeanspp.util.GenericFunctions1D;
+import eu.stratosphere.api.java.DataSet;
+import eu.stratosphere.api.java.ExecutionEnvironment;
+import eu.stratosphere.api.java.functions.GroupReduceFunction;
+import eu.stratosphere.api.java.tuple.Tuple3;
+import eu.stratosphere.util.Collector;
+
+public class KMeansppBagOfWords {
+	
+	public static class Document implements Serializable {
+		
+		private static final long serialVersionUID = -8646398807053061675L;
+		
+		public Map<String, Double> wordFreq;
+		public Integer id;
+		
+		public Document() {
+			id = -1;
+		}
+
+		public Document(Integer id) {
+			this.id = id;
+			this.wordFreq = new HashMap<String, Double>();
+		}
+		@Override
+		public String toString() {
+			String result = "docId:"+id;
+			for(Map.Entry<String, Double> itr : wordFreq.entrySet()) {
+				result += "|(" + itr.getKey() + ":" + itr.getValue() + ")";
+			}
+			return result;
+		}
+	}
+	
+	public static class IntegerWrapper implements Serializable {
+		private static final long serialVersionUID = 4295962833378918591L;
+		int value;
+		
+		public IntegerWrapper(int v) {
+			value = v;
+		}
+	}
+
+	public static final class RecordToDocConverter extends GroupReduceFunction<Tuple3<Integer, Integer, Double>, Document> {
+
+		private static final long serialVersionUID = -8476366121490468956L;
+
+		@Override
+		public void reduce(Iterator<Tuple3<Integer, Integer, Double>> values,
+				Collector<Document> out) throws Exception {
+			if(values.hasNext()) {
+				Tuple3<Integer, Integer, Double> elem = values.next();
+				Document doc = new Document(elem.f0);
+				doc.wordFreq.put(elem.f1.toString(), elem.f2);
+				
+				while(values.hasNext()) {
+					elem = values.next();
+					doc.wordFreq.put(elem.f1.toString(), elem.f2);
+				}
+				out.collect(doc);
+			}
+		}
+	}
+
+	public static class MyFunctions implements GenericFunctions1D<Document> {
+
+		private static final long serialVersionUID = 5510454279473390773L;
+		
+		private String pointsPath;
+		
+		public MyFunctions(String filePath) {
+			this.pointsPath = filePath;
+		}
+
+		@Override
+		public DataSet<Document> getDataSet(ExecutionEnvironment env) {
+			return env.readCsvFile(pointsPath).fieldDelimiter(' ')
+					.types(Integer.class, Integer.class, Double.class)
+					.groupBy(0).reduceGroup(new RecordToDocConverter());
+		}
+
+		@Override
+		public Document add(Document in1, Document in2) {
+			for(Map.Entry<String, Double> itr : in2.wordFreq.entrySet()) {
+				Double v1 = in1.wordFreq.get(itr.getKey());
+				if(v1 == null) {
+					in1.wordFreq.put(itr.getKey(), itr.getValue());
+				}
+				else {
+					in1.wordFreq.put(itr.getKey(), itr.getValue() + v1);
+				}
+			}
+			return in1;
+		}
+
+		@Override
+		public Document div(Document in1, long val) {
+			Document out = new Document(in1.id);
+			for(Map.Entry<String, Double> itr : in1.wordFreq.entrySet()) {
+				out.wordFreq.put(itr.getKey(), itr.getValue()/val);
+			}
+			return out;
+		}
+
+		@Override
+		public double distance(Document in1, Document in2) {
+			double sum = 0;
+			Set<String> added = new HashSet<String>();
+			
+			for(Map.Entry<String, Double> itr : in2.wordFreq.entrySet()) {
+				Double v1 = in1.wordFreq.get(itr.getKey());
+				double v2 = itr.getValue();
+				if(v1 == null) {
+					sum += v2 * v2;
+				}
+				else {
+					sum += (v2 - v1) * (v2 - v1);
+					added.add(itr.getKey());
+				}
+			}
+			
+			for(Map.Entry<String, Double> itr : in1.wordFreq.entrySet()) {
+				if(!added.contains(itr.getKey())) {
+					sum += itr.getValue() * itr.getValue();
+				}
+			}
+			return Math.sqrt(sum);
+		}
+
+	}
+
+	public static String getDescription() {
+		return "Parameters: <numSubTasks> <inputPath> <outputDirectory> <numClusters>  <maxIterations> <convergeCriteria>";
+	}
+	
+
+	public static void main(String[] args) throws Exception {
+		
+		if(args.length < 6) {
+			System.out.println(getDescription());
+			return;
+		}
+		int dop = Integer.parseInt(args[0]);
+		int k = Integer.parseInt(args[3]);
+		int itrs = Integer.parseInt(args[4]);
+		double epsilon = Double.parseDouble(args[5]);
+		KMeansppGeneric1D<Document> kmp = new KMeansppGeneric1D<Document>(dop, args[2], k, itrs, epsilon);
+		kmp.run(Document.class, new MyFunctions(args[1]));
+	}
+}
---
+++ impro3-ss14-stratosphere/src/main/java/de/tu_berlin/impro3/stratosphere/clustering/kmeanspp/KMeansppGeneric1D.java
@@ -0,0 +1,320 @@
+package de.tu_berlin.impro3.stratosphere.clustering.kmeanspp;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Random;
+
+import de.tu_berlin.impro3.stratosphere.clustering.kmeanspp.util.GenericFunctions1D;
+import eu.stratosphere.api.java.DataSet;
+import eu.stratosphere.api.java.ExecutionEnvironment;
+import eu.stratosphere.api.java.IterativeDataSet;
+import eu.stratosphere.api.java.aggregation.Aggregations;
+import eu.stratosphere.api.java.functions.FilterFunction;
+import eu.stratosphere.api.java.functions.GroupReduceFunction;
+import eu.stratosphere.api.java.functions.MapFunction;
+import eu.stratosphere.api.java.functions.ReduceFunction;
+import eu.stratosphere.api.java.tuple.Tuple2;
+import eu.stratosphere.api.java.tuple.Tuple3;
+import eu.stratosphere.configuration.Configuration;
+import eu.stratosphere.core.fs.Path;
+import eu.stratosphere.util.Collector;
+
+public class KMeansppGeneric1D<T> implements Serializable {
+
+	private static final long serialVersionUID = 3203582521287201233L;
+
+	public KMeansppGeneric1D(int dop, String outputPath, int numClusters, int maxIterations, double terminateEpsilon) {
+		this.dop = dop;
+		this.outputPath = outputPath;
+		this.numClusters = numClusters;
+		this.numIterations = maxIterations;
+		this.EPSILON = terminateEpsilon;
+	}
+
+	public void run(Class<T> typeClass, GenericFunctions1D<T> function) throws Exception {
+
+
+		// set up execution environment
+		ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+
+		// get input data
+		DataSet<T> points = function.getDataSet(env);
+		
+		// ========================== K-Means++ procedure ==================================
+
+		DataSet<Tuple2<Integer, T>> initial = points.reduce(new PickOnePoint()).map(new PointCentroidConverter());
+
+		IterativeDataSet<Tuple2<Integer, T>> addition = initial.iterate(numClusters - 1);
+
+		DataSet<Tuple2<T, Double>> distance = points.map(new SelectNearestDistance(function)).withBroadcastSet(addition, "centroids");
+
+		DataSet<Tuple2<T, Double>> sum = distance.aggregate(Aggregations.SUM, 1);
+
+		DataSet<Tuple2<Integer, T>> kmeansppTmpCenters = distance.reduceGroup(new PickWithDistance())
+			.withBroadcastSet(sum.cross(addition), "sumcrosscentroid")
+			.union(addition)
+			.map(new DummyMap());
+
+		DataSet<Tuple2<Integer, T>> kmeansInitCenters = addition.closeWith(kmeansppTmpCenters);
+		kmeansInitCenters.writeAsCsv(new Path(outputPath, "initcenters").toString(), "\n", "|");
+		
+		// ========================== standard K-Means procedure ==================================
+
+		// set number of bulk iterations for KMeans algorithm
+		IterativeDataSet<Tuple2<Integer, T>> iteration = kmeansInitCenters.iterate(numIterations);
+
+		DataSet<Tuple2<Integer, T>> kmeansTmpCenters = points
+			// compute closest centroid for each point
+			.map(new SelectNearestCenter(function)).withBroadcastSet(iteration, "centroids")
+				// count and sum point coordinates for each centroid
+			.map(new CountAppender())
+			.groupBy(0).reduce(new CentroidAccumulator(function))
+				// compute new centroids from point counts and coordinate sums
+			.map(new CentroidAverager(function));
+
+		// feed new centroids back into next iteration
+		DataSet<Tuple2<Integer, T>> finalCentroids = iteration.closeWith(
+				kmeansTmpCenters, 
+				// termination criterion
+				kmeansTmpCenters.join(iteration).where(0).equalTo(0).filter(new EpsilonFilter(function)));
+
+		DataSet<Tuple2<Integer, T>> clusteredPoints = points
+			// assign points to final clusters
+			.map(new SelectNearestCenter(function)).withBroadcastSet(finalCentroids, "centroids");
+
+		// emit result
+		clusteredPoints.writeAsCsv(new Path(outputPath, "points").toString(), "\n", "|");
+		finalCentroids.writeAsCsv(new Path(outputPath, "centers").toString(), "\n", "|");
+		// execute program
+		env.setDegreeOfParallelism(dop);
+		env.execute("KMeans++");
+
+	}
+
+	// *************************************************************************
+	//     USER FUNCTIONS
+	// *************************************************************************
+
+
+	/** Converts a Tuple3<Integer, Double,Double> into a Centroid. */
+	public final class PointCentroidConverter extends MapFunction<T, Tuple2<Integer, T>> {
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 6696423823857438943L;
+
+		@Override
+		public Tuple2<Integer, T> map(T p) throws Exception {
+			return new Tuple2<Integer, T>(1, p);
+		}
+	}
+
+	public final class DummyMap extends MapFunction<Tuple2<Integer, T>, Tuple2<Integer, T>> {
+
+		private static final long serialVersionUID = -8266144860463722495L;
+
+		@Override
+		public Tuple2<Integer, T> map(Tuple2<Integer, T> p) throws Exception {
+			return p;
+		}
+	}
+	public final class PickOnePoint extends ReduceFunction<T> {
+
+		private static final long serialVersionUID = -4173293469519514256L;
+
+		@Override
+		public T reduce(T point, T point2) throws Exception {
+			return point;
+		}
+	}
+
+	public final class PickWithDistance extends GroupReduceFunction<Tuple2<T, Double>, Tuple2<Integer, T>> {
+		
+		private static final long serialVersionUID = 2351745306551566932L;
+		private double sum;
+		private int maxId = 0;
+
+		@Override
+		public void open(Configuration parameters) throws Exception {
+			Collection<Tuple2<Tuple2<T, Double>, Tuple2<Integer, T>>> sumWrapper;
+			sumWrapper = getRuntimeContext().getBroadcastVariable("sumcrosscentroid");
+			for (Tuple2<Tuple2<T, Double>, Tuple2<Integer, T>> c: sumWrapper) {
+				sum = c.f0.f1;
+				if (c.f1.f0 > maxId) {
+					maxId = c.f1.f0;
+				}
+			}
+		}
+
+		@Override
+		public void reduce(Iterator<Tuple2<T, Double>> distance, Collector<Tuple2<Integer, T>> out) throws Exception {
+
+			Random r = new Random();
+			double pos = r.nextDouble() * sum;
+
+			while (distance.hasNext()) {
+				Tuple2<T, Double> d = distance.next();
+				pos -= d.f1;
+				if (pos < 0) {
+					out.collect(new Tuple2<Integer, T>(maxId + 1, d.f0));
+					return;
+				}
+			}
+		}
+	}
+
+	/** Determines the closest cluster center for a data point. */
+	public final class SelectNearestCenter extends MapFunction<T, Tuple2<Integer, T>> {
+
+		private static final long serialVersionUID = 3830298690315137146L;
+		private GenericFunctions1D<T> function;
+		private Collection<Tuple2<Integer, T>> centroids;
+
+		public SelectNearestCenter(GenericFunctions1D<T> function) {
+			this.function = function;
+		}
+		/** Reads the centroid values from a broadcast variable into a collection. */
+		@Override
+		public void open(Configuration parameters) throws Exception {
+			this.centroids = getRuntimeContext().getBroadcastVariable("centroids");
+		}
+
+		@Override
+		public Tuple2<Integer, T> map(T p) throws Exception {
+
+			double minDistance = Double.MAX_VALUE;
+			int closestCentroidId = -1;
+
+			// check all cluster centers
+			for (Tuple2<Integer, T> centroid : centroids) {
+				// compute distance
+
+				double distance = function.distance(p, centroid.f1);
+
+				// update nearest cluster if necessary
+				if (distance < minDistance) {
+					minDistance = distance;
+					closestCentroidId = centroid.f0;
+				}
+			}
+
+			// emit a new record with the center id and the data point.
+			return new Tuple2<Integer, T>(closestCentroidId, p);
+		}
+	}
+
+	/** Determines the closest cluster distance for a data point. */
+	public final class SelectNearestDistance extends MapFunction<T, Tuple2<T, Double>> {
+
+		private static final long serialVersionUID = -8867196536007432104L;
+		private Collection<Tuple2<Integer, T>> centroids;
+		private GenericFunctions1D<T> function;
+
+		public SelectNearestDistance(GenericFunctions1D<T> function) {
+			this.function = function;
+		}
+
+		/** Reads the centroid values from a broadcast variable into a collection. */
+		@Override
+		public void open(Configuration parameters) throws Exception {
+			this.centroids = getRuntimeContext().getBroadcastVariable("centroids");
+		}
+
+		@Override
+		public Tuple2<T, Double> map(T p) throws Exception {
+
+			double minDistance = Double.MAX_VALUE;
+
+			// check all cluster centers
+			for (Tuple2<Integer, T> centroid : centroids) {
+				// compute distance
+
+				double distance = function.distance(p, centroid.f1);
+
+				// update nearest cluster if necessary
+				if (distance < minDistance) {
+					minDistance = distance;
+				}
+			}
+			return new Tuple2<T, Double>(p, minDistance * minDistance);
+		}
+	}
+
+	/** Appends a count variable to the tuple. */
+	public final class CountAppender extends MapFunction<Tuple2<Integer, T>, Tuple3<Integer, T, Long>> {
+
+		private static final long serialVersionUID = -3743500564243984677L;
+
+		@Override
+		public Tuple3<Integer, T, Long> map(Tuple2<Integer, T> t) {
+			return new Tuple3<Integer, T, Long>(t.f0, t.f1, 1L);
+		}
+	}
+
+	/** Sums and counts point coordinates. */
+	public final class CentroidAccumulator extends ReduceFunction<Tuple3<Integer, T, Long>> {
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 3668504702360578838L;
+		private GenericFunctions1D<T> function;
+
+		public CentroidAccumulator(GenericFunctions1D<T> function) {
+			this.function = function;
+		}
+		@Override
+		public Tuple3<Integer, T, Long> reduce(Tuple3<Integer, T, Long> val1, Tuple3<Integer, T, Long> val2) {
+			return new Tuple3<Integer, T, Long>(val1.f0, function.add(val1.f1, val2.f1), val1.f2 + val2.f2);
+		}
+	}
+
+	/** Computes new centroid from coordinate sum and count of points. */
+	public final class CentroidAverager extends MapFunction<Tuple3<Integer, T, Long>, Tuple2<Integer, T>> {
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = -439825558772265542L;
+		private GenericFunctions1D<T> function;
+
+		public CentroidAverager(GenericFunctions1D<T> function) {
+			this.function = function;
+		}
+		@Override
+		public Tuple2<Integer, T> map(Tuple3<Integer, T, Long> value) {
+			T tmp = function.div(value.f1, value.f2);
+			return new Tuple2<Integer, T>(value.f0, tmp);
+		}
+	}
+	
+	public final class EpsilonFilter extends FilterFunction<Tuple2<Tuple2<Integer,T>,Tuple2<Integer,T>>> {
+		
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = -2106701714193572964L;
+		private GenericFunctions1D<T> function;
+		
+		public EpsilonFilter(GenericFunctions1D<T> function) {
+			this.function = function;
+		}
+
+		@Override
+		public boolean filter(Tuple2<Tuple2<Integer, T>, Tuple2<Integer, T>> value)
+				throws Exception {
+			System.out.println(String.format("Filter %d,%s with %d,%s", value.f0.f0, value.f0.f1.toString(), value.f1.f0, value.f1.f1.toString()));
+
+			return function.distance(value.f0.f1, value.f1.f1) > EPSILON;
+		}
+	}
+
+	private String outputPath = null;
+	private int dop;
+	private int numClusters;
+	private int numIterations;
+	
+	private double EPSILON = 0.001;
+}
